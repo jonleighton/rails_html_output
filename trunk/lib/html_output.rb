c1043097158e0b09c0b1f17f4f66f730647f306b
Jon Leighton <j@jonathanleighton.com>
2006-11-07 17:30:50 +0000

    Get rid of trunk

--- trunk/lib/html_output.rb
+++
@@ -1,8 +0,0 @@
-require 'active_support'
-
-module ActionView::Helpers::TagHelper
-  def tag(name, options = nil, open = false)
-    options = options.stringify_keys.delete_if { |k, option| option.to_s.empty? }
-    "<#{name}#{tag_options(options) if options}>"
-  end
-end
