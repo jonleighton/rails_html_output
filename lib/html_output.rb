1232b39aec099d203a8bf3f67f3dec077ca2dc34
Jon Leighton <j@jonathanleighton.com>
2007-12-11 19:39:26 +0000

    Update for Rails 2

--- lib/html_output.rb
+++ lib/html_output.rb
@@ -1,24 +1,16 @@
 require 'active_support'
 
 module ActionView::Helpers::TagHelper
-  
-  # First copy the standard tag method to tag_old
-  alias_method :tag_old, :tag
-  
-  # Calls tag_old making sure it will not self-close
-  def non_self_closing_tag(name, options = nil, open = false)
-    tag_old name, options, true
+  # Forces the "open" param to always be true so it never self-closes
+  def tag_with_non_self_closing(name, options = nil, open = false, *args)
+    tag_without_non_self_closing name, options, true
   end
   
-  # Now calls to tag will be non-self-closing
-  alias_method :tag, :non_self_closing_tag
-  
+  alias_method_chain :tag, :non_self_closing
 end
 
 class ActionView::Helpers::InstanceTag
-  
   # We have to re-alias tag_without_error_wrapping because it is previously
   # aliased to the old version of tag, which self-closes
-  alias_method :tag_without_error_wrapping, :non_self_closing_tag
-  
+  alias_method :tag_without_error_wrapping, :tag_with_non_self_closing
 end
--- test/test.rb
+++ test/test.rb
@@ -1,4 +1,4 @@
-%w(rubygems action_pack test/unit).each { |r| require r }
+%w(rubygems active_support action_controller action_view test/unit).each { |r| require r }
 require File.join(File.dirname(File.dirname(__FILE__)), 'lib', 'html_output')
 
 class HtmlOutputTest < Test::Unit::TestCase
@@ -6,6 +6,9 @@ class HtmlOutputTest < Test::Unit::TestCase
   
   def test_tag
     assert_not_self_closing tag("br")
+    assert_not_self_closing tag("link", :rel => "stylesheet")
+    assert_not_self_closing tag("link", {:rel => "stylesheet"}, true)
+    assert_not_self_closing tag("link", {:rel => "stylesheet"}, true, false)
   end
   
   def test_instance_tag
